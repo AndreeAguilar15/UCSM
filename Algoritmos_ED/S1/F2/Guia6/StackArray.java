fa1e73dd118b2ef7e0d5e1dad524d889b380ea3c
AndreeAguilar15 <143722715+AndreeAguilar15@users.noreply.github.com>
2025-05-06 11:53:59 -0500

    Create StackArray.java

---
+++ Algoritmos_ED/S1/F2/Guia6/StackArray.java
@@ -0,0 +1,48 @@
+package actividad1;
+
+public class StackArray<E> implements Stack<E> {
+    private E[] array;
+    private int tope;
+
+    public StackArray(int n) {
+        this.array = (E[]) new Object[n];
+        this.tope = -1;
+    }
+    public void push(E x) {
+        if (isFull()) {
+            throw new RuntimeException("La pila esta llena");
+        }
+        array[++tope] = x;
+    }
+    public E pop() throws ExceptionIsEmpty {
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty();
+        }
+        return array[tope--];
+    }
+    public E top() throws ExceptionIsEmpty {
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty();
+        }
+        return array[tope];
+    }
+    public boolean isEmpty() {
+        return this.tope == -1;
+    }
+    public boolean isFull() {
+        return this.tope == array.length - 1;
+    }
+    public String toString() {
+        if (isEmpty()) return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = tope; i >= 0; i--) {
+            sb.append(array[i]);
+            if (i > 0) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
