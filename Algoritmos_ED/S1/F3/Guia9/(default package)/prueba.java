b2b0790b404848e14be50cc98c12e0366d035aae
AndreeAguilar15 <143722715+AndreeAguilar15@users.noreply.github.com>
2025-06-08 13:51:13 -0500

    Update prueba.java

--- Algoritmos_ED/S1/F3/Guia9/(default package)/prueba.java
+++ Algoritmos_ED/S1/F3/Guia9/(default package)/prueba.java
@@ -1,1 +1,20 @@
+import graph.GraphLink;
 
+public class prueba {
+    public static void main(String[] args) {
+        GraphLink<Integer> graph = new GraphLink<>();
+
+        graph.insertVertex(1);
+        graph.insertVertex(2);
+        graph.insertVertex(3);
+        graph.insertVertex(4);
+
+        graph.insertEdge(1, 2);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
+        graph.insertEdge(3, 4);
+        graph.insertEdge(4, 1); 
+
+        graph.printList();
+    }
+}
